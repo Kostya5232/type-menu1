fdd0f508e8720ff8993a313b5b4cdc713d6dd950
Smo7e <120028063+Smo7e@users.noreply.github.com>
2023-08-20 11:24:59 +0400

    main

--- src/modules/Math3D/figures/OneWayHyperboloid.ts
+++ src/modules/Math3D/figures/OneWayHyperboloid.ts
@@ -37,6 +37,48 @@ class OneWayHyperboloid extends Figure {
                 polygons.push(new Polygon([i, i + 1 - count, i + 1, i + count], color));
             }
         }
+        const gradient = [
+            "#ff0000",
+            "#fd0c00",
+            "#fb1900",
+            "#f82600",
+            "#f53200",
+            "#f23e00",
+            "#ef4b00",
+            "#ed5700",
+            "#eb6400",
+            "#e87000",
+            "#e57d00",
+            "#e38900",
+            "#e09500",
+            "#dea200",
+            "#dcae00",
+            "#d9ba00",
+            "#d6c700",
+            "#d3d300",
+            "#d0df00",
+            "#cde600",
+            "#cae400",
+            "#c7e100",
+            "#c4de00",
+            "#c1db00",
+            "#bed800",
+            "#bbd500",
+            "#b8d200",
+            "#b5cf00",
+            "#b2cc00",
+            "#afc900",
+        ];
+        let index = 0;
+        while (index <= polygons.length) {
+            for (let i of gradient) {
+                for (let j = index; j <= index + count; j += 1) {
+                    if (j < polygons.length) polygons[j].color = polygons[0].hexToRgb(i);
+                }
+                index += count;
+            }
+            gradient.reverse();
+        }
 
         this.points = points;
         this.edges = edges;
